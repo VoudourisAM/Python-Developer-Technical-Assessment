b6be2226249ee296a5890cb95d97bc10275c441f
Angelos-Michail <79792887+VoudourisAM@users.noreply.github.com>
2025-10-25 11:44:14 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ---
+# ### Import Libraries
+# ---
+
+# In[3]:
+
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+# ---
+# ### models
+# ---
+
+# In[4]:
+
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(20), default="todo")
+    priority = db.Column(db.String(20), default="medium")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status,
+            "priority": self.priority,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
+
+
+# In[ ]:
+
+
+
+
